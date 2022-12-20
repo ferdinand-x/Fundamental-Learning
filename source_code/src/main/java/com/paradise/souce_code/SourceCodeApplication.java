a7c4f2608df8047409a22cf9d913669df28ed992
ferdinand-x <1123381476@qq.com>
2022-12-20 17:58:08 +0800

    init

---
+++ source_code/src/main/java/com/paradise/souce_code/SourceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.paradise.souce_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceCodeApplication.class, args);
+    }
+
+}
---
+++ source_code/src/test/java/com/paradise/souce_code/SouceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradise.souce_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
