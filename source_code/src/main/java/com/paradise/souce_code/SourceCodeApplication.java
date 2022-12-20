7990ff98cb2a48d03eab1b5f19ca977a47b9f0e0
ferdinand-x <1123381476@qq.com>
2022-12-20 22:18:43 +0800

    add author

--- source_code/src/main/java/com/paradise/souce_code/SourceCodeApplication.java
+++ source_code/src/main/java/com/paradise/souce_code/SourceCodeApplication.java
@@ -3,6 +3,9 @@ package com.paradise.souce_code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author PARADISE
+ */
 @SpringBootApplication
 public class SourceCodeApplication {
 
