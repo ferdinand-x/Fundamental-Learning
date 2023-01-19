4128dd265fd23b06b1ece79016ab3798831b3fba
ferdinand-x <1123381476@qq.com>
2023-01-19 17:09:46 +0800

    inject new dependency

--- source_code/src/main/java/com/paradise/source_code/SourceCodeApplication.java
+++ source_code/src/main/java/com/paradise/source_code/SourceCodeApplication.java
@@ -2,11 +2,12 @@ package com.paradise.source_code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author PARADISE
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SourceCodeApplication {
 
     public static void main(String[] args) {
