8b78a3ecd8156868aa89768840562e0ea059b0f9
ferdinand-x <1123381476@qq.com>
2023-06-08 22:10:29 +0800

    correct remove zipEntry api.

--- source-code/src/main/java/com/paradise/code/util/ExecutorCompletionServiceUtil.java
+++ source-code/src/main/java/com/paradise/code/util/ExecutorCompletionServiceUtil.java
@@ -1,14 +1,18 @@
 package com.paradise.code.util;
 
-import com.google.common.collect.Lists;
 import com.paradise.code.constant.ErrCodeEnum;
 import com.paradise.code.exception.VUtil;
 import com.paradise.code.pojo.dto.BaseRspDTO;
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.*;
 
+/**
+ * @author PARADISE
+ */
 @UtilityClass
 public class ExecutorCompletionServiceUtil {
 
@@ -19,24 +23,21 @@ public class ExecutorCompletionServiceUtil {
         VUtil.requireNotEmpty(tasks).throwErrorCode(ErrCodeEnum.TASKS_REQUIRE_NOT_EMPTY);
         VUtil.requireNoNull(executor).throwErrorCode(ErrCodeEnum.EXECUTOR_NO_NULL);
 
-        Collection<T> results = Lists.newArrayList();
+        List<T> results = new ArrayList<>(tasks.size());
         // submit task
         CompletionService<T> completionService = new ExecutorCompletionService<>(executor);
-//        for (Callable<BaseRspDTO<Object>> task : taskList) {
-//            completionService.submit(task);
-//        }
         tasks.forEach(completionService::submit);
 
         try {
             // transfer to result
             for (int i = 0; i < tasks.size(); i++) {
-                Future<T> baseRspDTOFuture = completionService.poll(timeOut, TimeUnit.SECONDS);
-                results.add(baseRspDTOFuture.get());
+                Future<T> future = completionService.poll(timeOut, TimeUnit.SECONDS);
+                results.add(future.get());
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e){
+            // ignore.
         }
 
         return results;
--- source-code/src/main/java/com/paradise/code/util/ZipTest3.java
+++ source-code/src/main/java/com/paradise/code/util/ZipTest3.java
@@ -14,9 +14,9 @@ import java.util.zip.ZipOutputStream;
 public class ZipTest3 {
 
     public static void main(String[] args) throws IOException {
-        Path srcZipPath = Path.of("C:\\Users\\GBA\\Desktop\\attachment\\TRF-TR-001_JF_20230605101822084\\TRF-TR-001_JF_20230605101822084\\D3B9C9AA03D544ACA7E4D1FD052D3E5D.zip");
+        Path srcZipPath = Path.of("C:\\Users\\PARADISE\\Desktop\\zip-test\\TRF-TR-001_JF_20230602131910339\\TRF-TR-001_JF_20230602131910339\\3C08E66A7E1947A69F6C2D52C0FEFEA8.zip");
         String resultFile = "result.json";
-        String resultJson = removeZipEntry(srcZipPath, resultFile);
+        String resultJson = removeZipEntry2(srcZipPath, resultFile);
         System.out.println(resultJson);
     }
 
@@ -49,9 +49,43 @@ public class ZipTest3 {
         entryIos.close();
 
         // overwrite source zip file
-        Files.copy(tempZipPath, srcZipPath, StandardCopyOption.REPLACE_EXISTING);
-        // delete temp zip file
-        Files.deleteIfExists(tempZipPath);
+        Files.move(tempZipPath, srcZipPath, StandardCopyOption.REPLACE_EXISTING);
         return resultJson;
     }
+
+    private static String removeZipEntry2(Path srcZipPath, String entryName) throws IOException {
+        Path tempZipPath = srcZipPath.getParent().resolve("temp.zip");
+        String resultJson = null;
+
+        try (ZipFile zipFile = new ZipFile(srcZipPath.toFile());
+             InputStream entryIos = zipFile.getInputStream(zipFile.getEntry(entryName));
+             ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(tempZipPath))) {
+
+            for (Enumeration<? extends ZipEntry> e = zipFile.entries(); e.hasMoreElements(); ) {
+                ZipEntry entryIn = e.nextElement();
+
+                if (entryName.equals(entryIn.getName())) {
+                    resultJson = new String(entryIos.readAllBytes(), StandardCharsets.UTF_8);
+                    continue;
+                }
+
+                ZipEntry entryOut = new ZipEntry(entryIn.getName());
+                zos.putNextEntry(entryOut);
+
+                try (InputStream is = zipFile.getInputStream(entryIn)) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = is.read(buffer)) != -1) {
+                        zos.write(buffer, 0, bytesRead);
+                    }
+                }
+
+                zos.closeEntry();
+            }
+        }
+
+        Files.move(tempZipPath, srcZipPath, StandardCopyOption.REPLACE_EXISTING);
+        return resultJson;
+    }
+
 }
