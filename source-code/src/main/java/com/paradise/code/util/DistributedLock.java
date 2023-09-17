c5910a70b63a8f3df88595848e6dd35b86ebb508
ferdinand-x <1123381476@qq.com>
2023-09-17 23:27:45 +0800

    redis DistributedLock renew expireTime.

--- source-code/src/main/java/com/paradise/code/util/DistributedLock.java
+++ source-code/src/main/java/com/paradise/code/util/DistributedLock.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,34 +16,57 @@ import java.util.concurrent.TimeUnit;
 @Component
 @RequiredArgsConstructor
 public class DistributedLock {
-    private static final long DEFAULT_EXPIRE_TIME = 30;
-    private static final long MAX_RETRY_TIMES = 3;
+    private static final long DEFAULT_EXPIRE_TIME = 10;
     private static final String LOCK_PREFIX = "GVN:LOCK:";
+
+    private static final long MAX_WAIT_TIME = 50000;
+    private static final ScheduledExecutorService SCHEDULED_EXECUTOR_SERVICE = Executors.newSingleThreadScheduledExecutor();
 
     private final ThreadLocal<String> lockHolder = new ThreadLocal<>();
 
     private final StringRedisTemplate redisTemplate;
 
-    public boolean acquireLock(String lockKey) throws InterruptedException {
-        return acquireLock(lockKey, DEFAULT_EXPIRE_TIME,TimeUnit.SECONDS);
+    public boolean acquireLock(String lockKey) {
+        return acquireLock(lockKey, DEFAULT_EXPIRE_TIME, TimeUnit.SECONDS);
     }
 
-    public boolean acquireLock(String lockKey, long expireTime,TimeUnit timeUnit) throws InterruptedException {
-        long expire = timeUnit.toSeconds(expireTime);
-        String key = LOCK_PREFIX + lockKey;
-        String value = UUID.randomUUID().toString();
-        long retryTimes = 0;
+    public boolean acquireLock(String key, long expireTime, TimeUnit timeUnit) {
+        long expireSec = timeUnit.toSeconds(expireTime);
+        String lockKey = LOCK_PREFIX + key;
+        String lockVal = UUID.randomUUID().toString();
 
-        while (retryTimes < MAX_RETRY_TIMES) {
-            Boolean success = redisTemplate.opsForValue().setIfAbsent(key, value, expire, TimeUnit.SECONDS);
-            if (success != null && success) {
-                lockHolder.set(value);
-                return true;
+        long startTime = System.currentTimeMillis();
+        long maxWaitTime = startTime + MAX_WAIT_TIME;
+
+        while (System.currentTimeMillis() < maxWaitTime) {
+            try {
+                Boolean success = redisTemplate.opsForValue().setIfAbsent(key, lockVal, expireSec, TimeUnit.SECONDS);
+                if (success != null && success) {
+                    lockHolder.set(lockVal);
+                    this.renewExpire(lockKey, lockVal, expireSec);
+                    return true;
+                }
+            } catch (Exception ignore) {
+
             }
-            retryTimes++;
-            TimeUnit.MILLISECONDS.sleep(50);
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
         return false;
+    }
+
+    private void renewExpire(String lockKey, String lockVal, long expireSec) {
+        long expireMs = TimeUnit.SECONDS.toMillis(expireSec);
+        long renewIntervalMs = expireMs / 3;
+        SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
+            String cacheVal = redisTemplate.opsForValue().get(lockKey);
+            if (Objects.equals(cacheVal, lockVal)) {
+                redisTemplate.opsForValue().set(lockKey, lockVal, expireSec, TimeUnit.SECONDS);
+            }
+        }, renewIntervalMs, renewIntervalMs, TimeUnit.MILLISECONDS);
     }
 
     public void releaseLock(String lockKey) {
